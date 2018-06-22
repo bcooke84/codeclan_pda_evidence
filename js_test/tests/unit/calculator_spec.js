a1fb3915c554ea803ee6450d0184735bd34e5131
Brian Cooke <brian.cooke@live.co.uk>
2018-06-22 18:45:48 +0100

    8 passing tests

--- js_test/tests/unit/calculator_spec.js
+++ js_test/tests/unit/calculator_spec.js
@@ -7,28 +7,28 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it should add 1 & 3 together', function(){
+  it('it should add 1 & 4 together', function(){
     calculator.previousTotal = 1;
-    calculator.add(2);
+    calculator.add(4);
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  it('it should subract 4 from 7', function() {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
     assert.equal(calculator.runningTotal, 3)
   })
 
-  it('it should subract 3 from 7', function() {
-    calculator.previousTotal = 7;
-    calculator.subtract(3);
-    assert.equal(calculator.runningTotal, 4)
+  it('it should multiply 3 & 5 together', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, 15)
   })
 
-  it('it should multiply 2 & 3 together', function(){
-    calculator.previousTotal = 2;
-    calculator.multiply(3);
-    assert.equal(calculator.runningTotal, 6)
-  })
-
-  it('it should divide 8 by 2', function() {
-    calculator.previousTotal = 8;
-    calculator.divide(2);
-    assert.equal(calculator.runningTotal, 4)
+  it('it should divide 21 by 7', function() {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(calculator.runningTotal, 3)
   })
 
   it('it should have a running total of 200', function() {
@@ -36,6 +36,35 @@ describe('calculator', function () {
     calculator.numberClick(0);
     calculator.numberClick(0);
     assert.equal(calculator.runningTotal, 200)
+  })
+
+  it('it should add 1 and  a running total of 200', function() {
+    calculator.numberClick(2);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    assert.equal(calculator.runningTotal, 200)
+  })
+
+  it('it should add 6 and 4, subtract 2, multiply by 3 and divide by 12 giving a running total of 2', function() {
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    calculator.numberClick(4);
+    calculator.operatorClick('-');
+    calculator.numberClick(2);
+    calculator.operatorClick('*');
+    calculator.numberClick(3);
+    calculator.operatorClick('/');
+    calculator.numberClick(12);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 2)
+  })
+
+  it('it should clear the runningTotal after numbers have been inputted', function() {
+    calculator.numberClick(3);
+    calculator.operatorClick('/');
+    calculator.numberClick(12);
+    calculator.clearClick();
+    assert.equal(calculator.runningTotal, 0)
   })
 
 });
